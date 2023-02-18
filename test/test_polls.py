aabb9dedc5b1580db0f0f21e6e64dae327e70a77
nguyenduchuy71 <wtfomg3650@gmail.com>
2023-02-18 10:53:27 +0700

    update test cases

--- test/test_polls.py
+++ test/test_polls.py
@@ -3,3 +3,6 @@ import unittest
 class TestIndexView(unittest.TestCase):
     def test_getqueryset(self):
         self.assertEqual(1, 1)
+
+    def test_getdemo(self):
+        self.assertEqual(1, 1)
